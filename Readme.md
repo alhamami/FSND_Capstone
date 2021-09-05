66f8cea36ce182bbfb3a80a38fb119697c14063b
alhamami <89762635+alhamami@users.noreply.github.com>
2021-09-05 21:12:52 +0300

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,39 @@
+# Capstone Project
+
+The restruant interface upholds a fundamental restruant by permitting clients to inquiry the information base to review the menu and also or to take a table and the users are 3 and unique client jobs and related consents are:
+
+- public client or user can see the menu and book a table and reservation director or manager can see an all reservations subtleties and menu administrator or manager can add or erase dishes from the menu.
+
+- The Auth0 information for endpoints that require authentication can be found in setup.sh. 
+
+## The API 
+The API link: https://jalalapp.herokuapp.com/
+
+
+## Installing Dependencies 
+
+1. **Python 3.7**
+ - Adhere to guidelines to introduce the most recent rendition of python for your foundation in the [python docs](https://docs.python.org/3/using/unix.html#getting-and-installing-the-latest-version-of-python)
+
+
+2. **Virtual Enviornment**
+ - We suggest working inside a virtual climate at whatever point utilizing Python for projects. This saves your conditions for each venture isolated and organaized. Guidelines for setting up a virual enviornment for your foundation can be found in the [python docs](https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/)
+
+
+3. **PIP Dependencies**
+ - When you have your virtual climate arrangement and running, introduce conditions by running:
+```bash
+pip install -r requirements.txt
+```
+This will introduce the entirety of the necessary bundles we chose inside the `requirements.txt` file.
+
+
+4. **Key Dependencies**
+ - [Flask](http://flask.pocoo.org/)  is a lightweight backend microservices system. Carafe is needed to deal with solicitations and reactions.
+
+ - [SQLAlchemy](https://www.sqlalchemy.org/) is the Python SQL toolbox and ORM we'll utilize handle the lightweight sqlite information base. You'll essentially work in app.py and can reference models.py.
+
+ - [Flask-CORS](https://flask-cors.readthedocs.io/en/latest/#) is the augmentation we'll use to deal with cross beginning solicitations from our frontend server.
+
+
+
---
+++ models.py
@@ -0,0 +1,181 @@
+import os
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime , timedelta
+import json
+
+
+
+postPath = 'postgres://mqtovrezzppmta:675f73bc68488f9ea8eafde79c3d467c4eea7e60aabe3d998476448a483480bc@ec2-44-195-201-3.compute-1.amazonaws.com:5432/dfe7co8k723b45'
+db = SQLAlchemy()
+
+def setup_db(app, database_path = postPath):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+class Table(db.Model):  
+  __tablename__ = 'table'
+
+  id = Column(Integer, primary_key=True)
+  chairs_num = Column(Integer)
+  reservation= db.relationship('Reservation', lazy=True)
+
+  def __init__(self, chairs_num ):
+    self.chairs_num = chairs_num
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'chairs_num': self.chairs_num,
+    }
+
+
+class Reservation(db.Model):  
+  __tablename__ = 'reservation'
+
+  id = Column(Integer, primary_key=True)
+  account =Column(String , nullable=False )
+  guest =Column(String , nullable=False )
+  table_id = Column(Integer, db.ForeignKey('table.id', ondelete='CASCADE'), nullable=False)
+  appointmentـTime = Column( db.DateTime , default=datetime.utcnow() + timedelta(days=1) ,nullable=False )
+
+  def __init__(self, account , guest , table_id , appointmentـTime ):
+    self.account = account
+    self.guest = guest
+    self.table_id = table_id
+    self.appointmentـTime = appointmentـTime
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'account':self.account,
+      'table_id': self.table_id,
+      'guest': self.guest,
+      'appointmentـTime':self.appointmentـTime
+    }
+
+
+class Menu(db.Model):  
+  __tablename__ = 'menu'
+
+  id = Column(Integer, primary_key=True)
+  dish_name = Column(String)
+  category = Column(String)
+  description = Column(String)
+  price = Column(String)   #make it integer
+
+  def __init__(self, dish_name, description, category, price ):
+    self.dish_name = dish_name
+    self.description= description
+    self.category = category
+    self.price =  price
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'dish_name': self.dish_name,
+      'description': self.description,
+      'category': self.category,
+      'price': self.price
+    }
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+    
+    table = Table(chairs_num = 1)
+    table.insert()
+
+    table = Table(chairs_num = 2)
+    table.insert()
+
+    table = Table(chairs_num = 3)
+    table.insert()
+
+    table = Table(chairs_num = 4)
+    table.insert()
+
+    table = Table(chairs_num = 5)
+    table.insert()
+
+    table = Table(chairs_num = 6)
+    table.insert()
+
+    table = Table(chairs_num = 8)
+    table.insert()
+
+
+    reservation = Reservation(account='jalal@hotmail.com', guest='jalal',table_id= 1,appointmentـTime= "2021-10-07 00:45:12")
+    reservation.insert()
+
+    reservation = Reservation(account='ali@gmail.com', guest='ali',table_id= 2,appointmentـTime= "2021-03-12 00:55:31")
+    reservation.insert()
+    
+
+    reservation = Reservation(account='ahmed@gmail.com', guest='ahmed',table_id= 3,appointmentـTime= "2021-11-34 00:18:27")
+    reservation.insert()
+
+
+    reservation = Reservation(account='mohmmed@gmail.com', guest='mohmmed',table_id= 3,appointmentـTime= "2021-06-22 00:22:14 ")
+    reservation.insert()
+
+    menu = Menu(dish_name = 'Big Mouth Burger',category = 'Burgers',description = "cheese, ketchup, mayonnaise",price = '15$')
+    menu.insert()
+
+    menu = Menu(dish_name = 'Beef Burger',category = 'Burgers',description = "ketchup, cheese, mayonnaise",price = '20$')
+    menu.insert()
+
+    menu = Menu(dish_name = 'Grilled Chicken Sandwich',category = 'Sandwiches',description = "ketchup",price = '23$')
+    menu.insert()
+
+    menu = Menu(dish_name = 'Seafood Sandwich',category = 'Sandwiches',description = "cheese",price = '16$')
+    menu.insert()
+
+    menu = Menu(dish_name = 'Cakes',category = 'Desserts',description = "Make the dough light",price = '12$')
+    menu.insert()
+
+    menu = Menu(dish_name = 'Confection',category = 'Desserts',description = "Make the dough light",price = '15$')
+    menu.insert()
+
+    menu = Menu(dish_name = 'Grape Juice',category = 'Juices',description = "little sugar",price = '10$')
+    menu.insert()
+
+    menu = Menu(dish_name = 'Carrot Juice',category = 'Juices',description = "More sugar",price = '17$')
+    menu.insert()
